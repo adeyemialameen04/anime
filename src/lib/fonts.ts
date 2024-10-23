62ae4afcd54cfc65f407eb7e632d7989dbca6990
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-24 00:16:49 +0100

    feat: added font and layed out the repo

---
+++ src/lib/fonts.ts
@@ -0,0 +1,7 @@
+import { DM_Sans } from "next/font/google";
+
+export const dmSans = DM_Sans({
+	variable: "--font-dm-sans",
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
