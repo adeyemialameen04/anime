2df9cdeb78919405622ec3cc18e4c797b531cb79
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-25 03:35:44 +0100

    feat: implemented anime detail with anilist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
 				},
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
+				ring: "transparent",
 				chart: {
 					"1": "hsl(var(--chart-1))",
 					"2": "hsl(var(--chart-2))",
